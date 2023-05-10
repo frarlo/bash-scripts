fe7ce12ab63528458c6b39dbc5c08262b6fcd3d0
FrArLo <72388105+xcutor8@users.noreply.github.com>
2023-05-10 19:08:43 +0200

    Create file_or_dir.sh

---
+++ file_or_dir.sh
@@ -0,0 +1,16 @@
+# Asking the user for the input:
+read -p "Enter the archive or file you want to search in this folder" object
+# loop between the folder's contents:
+for file in *
+do
+  if [[ $file == $object ]]; then   # The script has found the object name.
+    if [[ -d $file ]]; then         # The script checks if the object is a directory.
+    echo "$object is a directory."
+    elif [[ -f $file ]]; then       # The script checks if the object is a file.
+    echo "$object is a file."
+    fi
+  exit 0                            # The script returns a 0 status (success) and exits.
+  fi
+done
+
+echo "$object has not been found."  # The script has not found the enter object.
